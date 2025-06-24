d54e7a7757fa7f13c44ef08e2600f13e952a584a
Michael Maratita <118246054+michaelmaratita@users.noreply.github.com>
2025-06-24 01:35:42 -0700

    Update lambda_function.py

--- python/lambda_function.py
+++ python/lambda_function.py
@@ -1,13 +1,16 @@
-import start_stop as ss
+from start_stop.main import pre_check
+from start_stop.aws.sns_handler import SNSHandler
+
 
 def lambda_handler(event, context):
     if event['send_mail']:
-        ss.sns.send_mail(ss.sns(event))
+        sns = SNSHandler(event)
+        sns.send_mail()
         return {
             'statusCode': 200
         }
     elif event['next_phase']:
-        next_phase, initial_state = ss.pre_check(event)
+        next_phase, initial_state = pre_check(event)
         return {
             'statusCode': 200,
             'next_phase': next_phase,
